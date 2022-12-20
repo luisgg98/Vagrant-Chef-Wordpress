aae5c3e541e5a99487381d9230d90b94521d5c2f
luisgg98 <social4media1492@outlook.com>
2022-12-20 19:27:09 +0100

    kitchen test

--- cookbooks/wordpress/recipes/default.rb
+++ cookbooks/wordpress/recipes/default.rb
@@ -1,7 +1,13 @@
 # Chef recipes
 # https://docs.chef.io/workstation/ctl_chef/#chef-generate-recipe
-#apt_update "update_apt_cache"
+apt_update "update_apt_cache"
 
+execute "apt-get-update" do
+    command "apt-get update"
+    ignore_failure true
+    action :nothing
+end
+  
 include_recipe '::apache'
 include_recipe '::database'
 include_recipe '::wordpress'
--- cookbooks/wordpress/test/integration/wordpresssettings/serverspec/default_spec.rb
+++ cookbooks/wordpress/test/integration/wordpresssettings/serverspec/default_spec.rb
@@ -11,3 +11,15 @@ if os[:family] =='redhat'
     it { should exist }
   end
 end
+
+if os[:family] == 'ubuntu' 
+  describe command('curl http://0.0.0.0:80') do 
+      its(:stdout) { should match /""/ } 
+  end
+end
+
+if os[:family] =='redhat'
+  describe command('curl http://0.0.0.0:80') do 
+    its(:stdout) { should match /""/ } 
+  end
+end
